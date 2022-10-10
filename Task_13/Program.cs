b55faaa2e583db9b9342498101d33ef5b18c9052
Evgenia S <Dla-evgenii@yandex.ru>
2022-10-10 21:50:32 +0600

    Задача 13 (выведение третьей цифры заданного числа)

---
+++ Task_13/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+void TwoNum2()
+{
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+num = Math.Abs(num);
+
+    if (num > 99 )
+    {
+        while (num > 999)
+        {
+            num/=10;
+        }           
+        Console.WriteLine("Третья цифра числа " + num%10);
+    }
+    else
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+}
+
+TwoNum2 ();
