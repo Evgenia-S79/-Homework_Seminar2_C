89e479a5a338b450144f6d7dd31782bdd209123e
Evgenia S <Dla-evgenii@yandex.ru>
2022-10-10 21:51:38 +0600

    Задача 15 (выведение выходного или рабочего дня, по введенному числу)

---
+++ Task_15/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите число от 1 до 7: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+string WorkHoliday(int day)
+{
+    if (day > 0 && day < 8)
+    {
+        if (day == 6 || day == 7)
+        {
+            return ("Выходной день");
+        }
+        else
+        {
+            return ("Рабочий день");
+        }
+    }
+    else
+    {
+        return ("Ошибка. Не верное число!");
+    }
+}
+Console.WriteLine(WorkHoliday(day));
